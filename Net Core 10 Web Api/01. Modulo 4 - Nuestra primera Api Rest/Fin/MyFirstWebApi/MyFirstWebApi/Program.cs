c24d3442dfbc70398332af3c1f5ab2d334fe92a5
jordiascension <jordiascension@enterpriseconsulting.es>
2025-11-25 16:51:49 +0100

    Implementación de nuestra primera Api con un CRUD de TodoItems

--- Net Core 10 Web Api/01. Modulo 4 - Nuestra primera Api Rest/Fin/MyFirstWebApi/MyFirstWebApi/Program.cs
+++ Net Core 10 Web Api/01. Modulo 4 - Nuestra primera Api Rest/Fin/MyFirstWebApi/MyFirstWebApi/Program.cs
@@ -1,3 +1,5 @@
+using MyFirstWebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<TodoRepository>();
 
 var app = builder.Build();
 
