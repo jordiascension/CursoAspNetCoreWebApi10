0384c2f493740dda5aa1602554bc8360ff29dcc2
jordiascension <jordiascension@enterpriseconsulting.es>
2025-11-29 00:38:51 +0100

    Generar documentación en Inglés

--- Net Core 10 Web Api/01. Modulo 4 - Nuestra primera Api Rest/Fin/MyFirstWebApi/MyFirstWebApi/Program.cs
+++ Net Core 10 Web Api/01. Modulo 4 - Nuestra primera Api Rest/Fin/MyFirstWebApi/MyFirstWebApi/Program.cs
@@ -1,37 +1,34 @@
+using System.Reflection.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-// Nuevo generador OpenAPI de ASP.NET Core
 builder.Services.AddOpenApi(options =>
 {
-    // Opcional: personalizar el documento
-    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    options.AddDocumentTransformer((document, context, CancellationToken) =>
     {
-        document.Info.Title = "Mi API con Controladores";
+        document.Info.Title = "My First Web Api";
         document.Info.Version = "v1";
-        document.Info.Description = "Ejemplo usando AddOpenApi + Swagger UI en .NET 10";
+        document.Info.Description = "This is my first web API using ASP.NET Core.";
         return Task.CompletedTask;
     });
 });
-
 builder.Services.AddSingleton<MyFirstWebApi.Repositories.TodoItemRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    // Swagger UI (Swashbuckle) apuntando al documento generado por AddOpenApi
     app.UseSwaggerUI(options =>
     {
-        // Ruta del JSON que expone MapOpenApi
-        options.SwaggerEndpoint("/openapi/v1.json", "Mi API v1");
-        options.RoutePrefix = "";   
-        options.DocumentTitle = "Mi Swagger UI";
+        options.SwaggerEndpoint("/openapi/v1.json", "My First Web Api v1");
+        options.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+        options.DocumentTitle = "My First Web Api Documentation";
     });
 }
 
