8b7f06e4daffeefe7c4966a91b152fa5ad79d1c3
jordiascension <jordiascension@enterpriseconsulting.es>
2025-12-02 17:06:41 +0100

    Implementación de cliente http con nuestra web api

---
+++ Net Core 10 Web Api/01. Modulo 4 - Nuestra primera Api Rest/Fin/MyFirstWebApi/MyFirstWebApi.BlazorApp/Program.cs
@@ -0,0 +1,34 @@
+using MyFirstWebApi.BlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register HttpClientFactory
+builder.Services.AddHttpClient("MyFirstWebApi", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5254/api/"); // API URL
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
