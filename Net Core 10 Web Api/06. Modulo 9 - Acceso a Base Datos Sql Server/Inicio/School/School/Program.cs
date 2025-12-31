b7c5994d4eb7881f5fbc433178abd4a64db426b5
jordiascension <jordiascension@enterpriseconsulting.es>
2025-12-31 20:56:40 +0100

    Implementación de Docker Compose con Visual Studio y HealthCheck Visual de Sql Server (Sirve como plantilla)

---
+++ Net Core 10 Web Api/06. Modulo 9 - Acceso a Base Datos Sql Server/Inicio/School/School/Program.cs
@@ -0,0 +1,67 @@
+﻿using HealthChecks.UI.Client;
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services
+    .AddHealthChecks()
+    .AddSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")!,
+        name: "sqlserver",
+        timeout: TimeSpan.FromSeconds(5),
+        failureStatus: HealthStatus.Unhealthy
+    );
+
+builder.Services
+    .AddHealthChecksUI(setup =>
+    {
+        setup.AddHealthCheckEndpoint("API + SQL", "/healthz");
+        setup.SetEvaluationTimeInSeconds(10);
+        setup.SetMinimumSecondsBetweenFailureNotifications(60);
+    })
+    .AddInMemoryStorage();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// ✅ Endpoint para UI y para Docker/K8s (formato correcto UI)
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    },
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecksUI(options =>
+{
+    options.UIPath = "/health-ui";
+    options.ApiPath = "/health-ui-api";
+});
+
+
+app.Run();
